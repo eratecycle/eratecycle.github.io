fe04f85227c1257750388e8dbe37d2d784c16e4f
Nick Nance <nance.nick@gmail.com>
2015-03-24 20:53:30 -0500

    added inpinia styles and added signup form

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var spawn = require('child_process').spawn;
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
@@ -21,8 +22,12 @@ gulp.task('jekyll', ['sass', 'bower'], function () {
 
 // Compile SASS
 gulp.task('sass', function(cb) {
-  return sass('_scss/styles.scss', { sourcemap: true })
+  return gulp.src('_scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('_site/css'))
     .pipe(reload({stream: true}, cb));
 });
 
@@ -48,7 +53,7 @@ gulp.task('serve', ['jekyll'], function() {
 // Watch for changes
 gulp.task('watch', function () {
     // Manually compile and inject css to avoid jekyll overhead, and utilize livereload injection
-    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch('_scss/**', ['sass']);
 
     // Watch for changes to other files for jekyll compilation
     // Note: This will probably need to be updated with the files you want to watch
---
+++ js/form.js
@@ -0,0 +1,10 @@
+// INSPINIA Form spcific JS
+$(document).ready(function () {
+
+    $('.navbar').addClass('navbar-scroll');
+
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+    });
+});
