d08b1f4c95afeff47649e6f6a4e3c1423f4f53ce
Nick Nance <nance.nick@gmail.com>
2015-03-29 18:01:17 -0500

    make dropzone requirable with browserify

--- _includes/portal/js/views/file-upload.js
+++ _includes/portal/js/views/file-upload.js
@@ -1,7 +1,35 @@
 var Backbone = require('backbone');
+var Dropzone = require('dropzone');
 
 module.exports = Backbone.View.extend({
 
-  template: require('../templates/file-upload.jst')
+  template: require('../templates/file-upload.jst'),
+
+  onShow: function() {
+    var myDropZone = new Dropzone('.dropzone', {
+
+      autoProcessQueue: false,
+      uploadMultiple: true,
+      parallelUploads: 10,
+      maxFiles: 10,
+
+      // Dropzone settings
+      init: function() {
+          var myDropzone = this;
+
+          this.element.querySelector('button[type=submit]').addEventListener('click', function(e) {
+              e.preventDefault();
+              e.stopPropagation();
+              myDropzone.processQueue();
+          });
+          this.on('sendingmultiple', function() {
+          });
+          this.on('successmultiple', function(files, response) {
+          });
+          this.on('errormultiple', function(files, response) {
+          });
+      }
+    });
+  }
 
 });
