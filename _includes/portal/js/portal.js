1a1d6979919e8189a1e0a369727a0c5c34a57855
Nick Nance <nance.nick@gmail.com>
2015-04-07 14:37:41 -0500

    enhance security control

--- _includes/portal/js/portal.js
+++ _includes/portal/js/portal.js
@@ -11,6 +11,7 @@ Backbone.View.prototype.attachToTemplate = true;
 
 $(document).ready(function () {
 
+  // send all ajax request to the correct backbend api server
   $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
     if (location.hostname.indexOf('localhost') !== -1) {
       options.url = 'http://localhost:9010' + options.url;
@@ -19,22 +20,27 @@ $(document).ready(function () {
     }
   });
 
-  var creds = sessionStorage.getItem('eratecycle');
-  if (creds) {
-    creds = JSON.parse(creds);
-    $.ajaxSetup({
-        headers: { 'Authorization': 'Basic ' + btoa(creds.email + ':' + creds.password) }
-    });
-  }
-
-  var mainView = new MainView({el: 'body'});
-  var router = new Router();
-
-  user.fetch().fail(function(xhr, textStatus, err){
+  // if we get a 401 from any request immediatly log off
+  $(document).ajaxComplete(function(e, xhr, settings) {
     if (xhr.status == 401) {
       location.href = '/login';
     }
   });
+
+  // add basic auth creds to all ajax requests
+  var creds = sessionStorage.getItem('eratecycle');
+  if (creds) {
+    creds = JSON.parse(creds);
+    $.ajaxSetup({
+      headers: { 'Authorization': 'Basic ' + btoa(creds.email + ':' + creds.password) }
+    });
+  }
+
+  user.fetch();
+
+  var mainView = new MainView({el: 'body'});
+  var router = new Router();
+
   broker.start();
   Backbone.history.start();
 });
