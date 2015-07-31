8c9ab913f22fc71ffad2f0483f56f4033f476e3e
Jacob Madden <jacob.madden@gmail.com>
2015-07-31 10:48:59 -0400

    updates to file-manager in prep for filtering files by clicks

--- _includes/portal/js/views/file-manager.js
+++ _includes/portal/js/views/file-manager.js
@@ -5,6 +5,12 @@ module.exports = Backbone.View.extend({
 
   template: require('../templates/file-manager.jst'),
 
+  events: {
+    'click #files-all': 'clickAllFiles',
+    'click #files-invoices': 'clickInvoices',
+    'click #files-forms': 'clickForms'
+  },
+
   initialize: function() {
     this.listenTo(this.collection,'add',this.addItem);
     this.listenTo(this.collection,'remove',this.removeSubViewForModel);
@@ -16,6 +22,21 @@ module.exports = Backbone.View.extend({
       view: new FileItemView({model:model}),
       selector: '#file-list'
     });
+  },
+
+  clickAllFiles: function(evt) {
+    evt.preventDefault();
+    console.log('clickAllFiles');
+  },
+
+  clickInvoices: function(evt) {
+    evt.preventDefault();
+    console.log('clickInvoices');
+  },
+
+  clickForms: function(evt) {
+    evt.preventDefault();
+    console.log('clickForms');
   }
 
 });
